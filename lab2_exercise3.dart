f7dd1cdcb2e9e09c48efc2522aa1606ff852a8af
phichetchai67 <phichetchais67@nu.ac.th>
2026-06-29 12:09:02 +0700

    first commit

---
+++ lab2_exercise3.dart
@@ -0,0 +1,23 @@
+void main() {
+  List<String> subject = ['math','si','com'];
+  print(subject.length);
+  print(subject[0]);
+  print(subject[2]);
+  
+  subject.add('eng');
+  print(subject);
+  
+  Map<String, int> studentScores = {
+    'match': 20,
+    'si': 55,
+    'com': 60
+  };
+  print(studentScores['si']);
+  
+  studentScores['eng'] = 99;
+  print(studentScores);
+  print(studentScores.keys);
+  print(studentScores.values);
+  
+  
+}
