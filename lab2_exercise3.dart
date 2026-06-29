9eace6d8e57300b992039dd71578ee2f4b94fc62
phichetchai67 <phichetchais67@nu.ac.th>
2026-06-29 12:42:25 +0700

    h

--- lab2_exercise3.dart
+++ lab2_exercise3.dart
@@ -1,29 +1,31 @@
 void main() {
   List<String> subject = ['math','si','com'];
-  print(subject.length);
-  print(subject[0]);
-  print(subject[2]);
+  print('Number of subjects: ${subject.length}');
+  print('First subject: ${subject[0]}');
+  print('Last subject: ${subject[2]}');
   
   subject.add('eng');
-  print(subject);
+  print('Updated subjects: $subject');
   
   Map<String, int> studentScores = {
     'match': 20,
     'si': 55,
     'com': 60
   };
-  print(studentScores['si']);
+  print('Score for si: ${studentScores['si']}');
   
   studentScores['eng'] = 99;
-  print(studentScores);
-  print(studentScores.keys);
-  print(studentScores.values);
+  print('Updated scores: $studentScores');
+  print('All subjects in map: ${studentScores.keys}');
+  print('All scores in map: ${studentScores.values}');
 
   studentScores.forEach((subject,score){
     if (subject.toLowerCase().contains('a')){
+    print('subject "a" ');
     print('$subject');
   }
   });
+  print('subject score > 50');
   studentScores.forEach((subject,score){
     if (score > 50){
     print('$subject : $score');
